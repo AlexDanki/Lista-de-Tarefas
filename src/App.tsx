671fd687a1ad74a4fba975999f2d6ca8d93c0e4d
Alexsandro Duarte <pocmusic321@gmail.com>
2025-08-10 21:22:00 -0300

    Estilizando pag

--- src/App.tsx
+++ src/App.tsx
@@ -26,24 +26,32 @@ function App() {
 
         <div className="content-box">
 
-          <h1>Lista de Tarefas</h1>
+          
 
           <section className='content-main'>
 
-            <input 
-              type="text" 
-              value={input}
-              onChange={(e)=> setInput(e.target.value)}
-            />
+            <div className="content-head">
+              <h1>Lista de Tarefas</h1>
 
-            <button onClick={handleAdd}>Adicionar tarefa</button>
+              <div className='input-button'> 
+                <input 
+                  type="text" 
+                  value={input}
+                  onChange={(e)=> setInput(e.target.value)}
+                  placeholder='Digite a tarefa'
+                />
 
+                <button onClick={handleAdd}>ADICIONAR</button>
+              </div>
+             
+            </div>
+            
             <div className="list-area">
 
               {
                 tasks && tasks.map((item, index) => (
                   <div className="item" key={index}>
-                    <span>{index + 1}. {item}</span>
+                    <span className='item-txt'>{index + 1}. {item}</span>
                     <button>Remover</button>
                     <button>Editar</button>
                   </div>
